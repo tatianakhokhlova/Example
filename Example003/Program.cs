c462a6c8d35032144b88eff0ae902dc0139b28a4
Tatyana <tatyana.dovganskaya@gmail.com>
2022-09-29 21:31:13 +0300

    Изменила информацию в примере Example003

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,4 +1,7 @@
-﻿int numberA = 3;
-int numberB = 5;
-Console.WriteLine(numberA+numberB);
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
 
